161084873c8423bdb52c0631d52339853e2e8a2f
AcePhrost <cameronlewis2019@gmail.com>
2024-01-25 09:34:38 -0600

    some errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+export default function app(){
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const cars = [
+    {
+    "model" : "G37",
+    "make" : "infinti",
+    "year" : '2009',
+    "car_id" : '1'
+    },
+    {
+    "model" : "SS",
+    "make" : "impala",
+    "year" : '2008',
+    "car_id" : '2'
+    },
+    {
+    "model" : "M35",
+    "make" : "infinti",
+    "year" : '2010',
+    "car_id" : '3'
+    },
+  ]
+    return ( 
+      <>
+        <h1>Car Lot</h1>
+        { cars.length > 0 ? cars.map(( car ) => {
+          return <Cars key={car.car_id} car={car} />
+        }) : <p> No cars in Inventoy</p> }
+      </>  
+      )
 }
-
-export default App
---
+++ src/componets/Cars.jsx
@@ -0,0 +1,9 @@
+
+export default function Cars(props) {
+  const {cars} = props
+  return (
+    <p>
+      {cars.model}
+    </p>
+  )
+}
